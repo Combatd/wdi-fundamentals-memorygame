a7fd6818b53411586fb5732b64c675f4631df353
combatd <combatdstar@gmail.com>
2019-08-24 09:22:00 -0700

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,7 +1,21 @@
-console.log('Up and running!');
-var cardOne = 'queen';
-var cardTwo = 'queen';
-var cardThree = 'king';
-var cardFour = 'king';
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardFour);
+// array to store four cards
+var cards = ['queen', 'queen', 'king', 'king'];
+var cardsInPlay = []; // empty array
+
+// represent cards user flipped
+var cardOne = cards[0]; // 1st ele of cards array
+var cardTwo = cards[2]; // 2nd ele of cards array
+
+cardsInPlay.push(cardOne); // push to end of cardsInPlay
+console.log('User flipped ' + cardOne);
+cardsInPlay.push(cardTwo);
+console.log('User flipped ' + cardTwo);
+
+// check if there is 2 elements in cardsInPlay
+if (cardsInPlay.length === 2) {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You found a match!");
+	} else {
+		alert("Sorry, try again.");
+	}
+}
